ad8cc0766d4d631da28ef5b018ecce34761fbc68
Carl Fremault <carlfremault@yahoo.com>
2024-05-07 20:22:06 +0200

    Tailwind customize theme - font, colors
    Add header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="mx-auto w-100 bg-light-grey py-6">
+      <h1 className="text-center text-2xl font-bold">
+        Kairon Labs - Technical Assignment
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
