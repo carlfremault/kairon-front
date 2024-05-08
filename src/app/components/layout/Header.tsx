0f5213b322af7ddbcc0bf99b1860facd5a60aded
Carl Fremault <carlfremault@yahoo.com>
2024-05-09 00:59:39 +0200

    responsiveness header & line break fixed

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -2,7 +2,9 @@ const Header = () => {
   return (
     <div className="w-100 mx-auto bg-light-grey py-6">
       <h1 className="text-center text-2xl font-bold">
-        Kairon Labs - Technical Assignment
+        <span className="block sm:inline">Kairon Labs</span>
+        <span className="block sm:inline"> - </span>
+        <span className="block sm:inline">Technical Assignment</span>
       </h1>
     </div>
   );
