503a21c8468abe5b6a16252965a2bd1553f2a7a3
Carl Fremault <carlfremault@yahoo.com>
2024-05-11 18:13:10 +0200

    status message styling

--- src/app/components/utils/DashboardStatusMessage.tsx
+++ src/app/components/utils/DashboardStatusMessage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const DashboardStatusMessage = ({ statusText }: { statusText: string }) => {
-  return <p className="mt-20 text-center text-xl">{statusText}</p>;
+  return (
+    <p className="mt-20 w-full border border-dark-grey bg-light-grey py-20 text-center text-xl">
+      {statusText}
+    </p>
+  );
 };
 
 export default DashboardStatusMessage;
