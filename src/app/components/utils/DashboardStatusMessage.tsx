73109ed9877e37ee03f94c273be1cbb8697793ef
Carl Fremault <carlfremault@yahoo.com>
2024-05-11 21:20:28 +0200

    cleanup

--- src/app/components/utils/DashboardStatusMessage.tsx
+++ src/app/components/utils/DashboardStatusMessage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const DashboardStatusMessage = ({ statusText }: { statusText: string }) => {
   return (
     <p className="mt-20 w-full border border-dark-grey bg-light-grey py-20 text-center text-xl">
